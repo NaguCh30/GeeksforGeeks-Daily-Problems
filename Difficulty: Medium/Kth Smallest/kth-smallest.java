2d94d943ea72c60244874ef9316cb164864b2bb7
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-08-14 10:39:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int kthSmallest(int[] arr, int k) {
+        // Code here
+        PriorityQueue<Integer> maxHeap = 
+            new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int num : arr) {
+            maxHeap.offer(num);
+            
+            if(maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        
+        return maxHeap.peek();
+        
+    }
+}
