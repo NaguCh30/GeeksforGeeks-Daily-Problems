6f6782fb256ce00de8b1405437c7bb27adc17c53
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-08-16 17:57:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        String[] words = s.split("\\.+");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+
+            if (words[i].isEmpty()) {
+                continue;
+            }
+
+            if (sb.length() > 0) {
+                sb.append(".");
+            }
+
+            sb.append(words[i]);
+        }
+
+        return sb.toString();
+    }
+}
