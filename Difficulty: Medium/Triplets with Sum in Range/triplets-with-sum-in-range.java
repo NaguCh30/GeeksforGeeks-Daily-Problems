2943ff378c3323af7914adb1f5a855f9c82523b5
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-08-19 10:02:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplets with Sum in Range/triplets-with-sum-in-range.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int countTriplets(int[] arr, int l, int r) {
+        Arrays.sort(arr);
+
+        long ans = countLessOrEqual(arr, r) - countLessOrEqual(arr, l - 1);
+
+        return (int) ans;
+    }
+
+    private long countLessOrEqual(int[] arr, int target) {
+        int n = arr.length;
+        long count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                long sum = (long) arr[i] + arr[left] + arr[right];
+
+                if (sum <= target) {
+                    count += right - left;
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return count;
+    }
+}
