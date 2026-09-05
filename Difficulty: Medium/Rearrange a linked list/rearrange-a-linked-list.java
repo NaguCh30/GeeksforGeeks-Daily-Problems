2ee371257686989268e297dfda88fdb449d6516f
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-05 11:57:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rearrange a linked list/rearrange-a-linked-list.java
@@ -0,0 +1,31 @@
+/* Node Structure
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+} */
+
+class Solution {
+    public void rearrangeEvenOdd(Node head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+    }
+}
