e9b1e5f85f50f9302e0b8e9d24c35a6a8184afc6
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-06 10:20:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Pairwise ANDs/sum-of-pairwise-ands.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long pairAndSum(int[] arr) {
+        // code here
+        long ans = 0;
+        
+        for (int bit = 0; bit < 32; bit++) {
+        
+            long count = 0;
+            
+            for (int num : arr) {
+                if ((num & (1L << bit)) != 0) {
+                    count++;
+                }
+            }
+            
+            long pairs = count * (count - 1) / 2;
+            
+            ans += pairs * (1L << bit);
+        }
+        
+        return ans;
+    }
+}
