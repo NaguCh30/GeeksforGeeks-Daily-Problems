d6e046b10905aa3ba97201f870ee07df74ef33d7
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-08 09:47:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Word in Grid - All Occurrences/word-in-grid-all-occurrences.java
@@ -0,0 +1,48 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> searchWord(char[][] mat, String word) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        int n = mat.length;
+        int m = mat[0].length;
+        int len = word.length();
+
+        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+
+                if (mat[r][c] != word.charAt(0)) {
+                    continue;
+                }
+
+                for (int d = 0; d < 8; d++) {
+                    int nr = r;
+                    int nc = c;
+                    int k;
+
+                    for (k = 1; k < len; k++) {
+                        nr += dr[d];
+                        nc += dc[d];
+
+                        if (nr < 0 || nr >= n || nc < 0 || nc >= m
+                                || mat[nr][nc] != word.charAt(k)) {
+                            break;
+                        }
+                    }
+
+                    if (k == len) {
+                        ArrayList<Integer> pos = new ArrayList<>();
+                        pos.add(r);
+                        pos.add(c);
+                        ans.add(pos);
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
