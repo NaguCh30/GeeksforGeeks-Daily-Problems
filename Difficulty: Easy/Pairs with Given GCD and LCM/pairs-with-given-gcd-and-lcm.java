45d0b865e5586ff169ac2497dbac5a2abab296b2
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-10 10:50:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with Given GCD and LCM/pairs-with-given-gcd-and-lcm.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int pairCount(int x, int y) {
+
+        if (y % x != 0) {
+            return 0;
+        }
+
+        int n = y / x;
+        int distinctPrimeFactors = 0;
+
+        for (int i = 2; i * i <= n; i++) {
+
+            if (n % i == 0) {
+                distinctPrimeFactors++;
+
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+
+        if (n > 1) {
+            distinctPrimeFactors++;
+        }
+
+        return 1 << distinctPrimeFactors;
+    }
+}
