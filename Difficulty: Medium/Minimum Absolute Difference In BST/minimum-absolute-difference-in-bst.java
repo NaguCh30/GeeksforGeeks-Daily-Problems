b92ba1ece77497bf710fe8ca7259a97265451996
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-18 20:33:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Absolute Difference In BST/minimum-absolute-difference-in-bst.java
@@ -0,0 +1,42 @@
+/* The Node structure is defined as
+ class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+
+    int prev = -1;
+    int min = Integer.MAX_VALUE;
+
+    public int absDiff(Node root) {
+        inorder(root);
+        return min;
+    }
+
+    private void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        // Left
+        inorder(root.left);
+
+        // Current
+        if (prev != -1) {
+            min = Math.min(min, root.data - prev);
+        }
+
+        prev = root.data;
+
+        // Right
+        inorder(root.right);
+    }
+}
